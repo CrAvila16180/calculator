c53ecbc7adf3aed8aefd49a1cbc9146248d932d6
CR_Avila <carlosrobertoavilah@gmail.com>
2019-01-04 18:34:11 -0600

    Perform Basic operations

--- main.js
+++ main.js
@@ -1,26 +1,123 @@
-let viewer = document.querySelector('.displayNum'); //Where numberis displayed
-let equal = document.querySelector('.equal'); //Equal button
-let nums = Array.from(document.querySelectorAll('.number')) //NUmber buttons
-let ops = Array.from(document.querySelectorAll('.operation')) //ops buttons
+let viewer = document.querySelector('.displayNum'); 
+let equal = document.querySelector('.equal'); 
+let nums = Array.from(document.querySelectorAll('.number')) 
+let ops = Array.from(document.querySelectorAll('.operation')) 
+let pendNum = document.querySelector('.prev');
+let clearButton = document.querySelector('.del');
 
-let theNum = ""; //Current number
-let oldNum = ""; //First Number
-let resultNum; //result
+let theNum = ""; 
+let oldNum = ""; 
+let resultNum; 
 let operator; 
+
+let addition = (x,y) => x+y;
+
+let substraction = (x,y) => x-y;
+
+multiplication = (x,y) => x*y;
+
+division = (x,y) => x/y;
+
+sqrt = (x) => Math.sqrt(x);
+
+pow = (x,y) => Math.pow(x,y);
+
+let factorial = (x) => {
+    let total = 1;
+    for(i = x; i > 0; i--){
+        total *= i;
+    };
+    return total;
+};
 
 let setNum = (e) => {
 
-    if (resultNum) {
-        theNum = e.target.getAttribute('data-value');
-    } else {
+    
         theNum += e.target.getAttribute('data-value');
-    }
+    
 
     viewer.textContent = theNum;
-    console.log(this)
+    console.log(theNum)
+};
+
+let moveNum = (e) => {
+    oldNum = theNum;
+    theNum = '';
+    operator = e.target.getAttribute('data-op');
+    console.log(operator);
+    pendNum.textContent = `${oldNum} ${e.target.textContent}`;
+
+    equal.setAttribute("data-result", "");
 }
 
-nums.forEach(num => num.addEventListener('click', setNum))
+const displayNumber = () => {
+    oldNum = parseFloat(oldNum);
+    theNum = parseFloat(theNum);
+
+    switch (operator){
+        case 'addition':
+        resultNum = addition(oldNum,theNum);
+        break;
+
+        case 'substraction':
+        resultNum = substraction(oldNum,theNum);
+        break;
+
+        case 'multiplication':
+        resultNum = multiplication(oldNum,theNum);
+        break;
+
+        case 'division':
+        resultNum = division(oldNum,theNum);
+        break;
+
+        case 'sqrt':
+        resultNum = sqrt(theNum);
+        break;
+
+        case 'pow':
+        resultNum = pow(theNum);
+        break;
+
+        case 'factorial':
+        resultNum = factorial(theNum);
+        break;
+
+        default:
+        resultNum = theNum;
+    }
+
+    if (!isFinite(resultNum)){
+        if (isNaN(resultNum)){
+            resultNum = 'rip';
+        } else {
+            resultNum = 'rip';
+
+        }
+    }
+    console.log(resultNum);
+    viewer.innerHTML = resultNum;
+    equal.setAttribute('data-result', resultNum);
+    oldNum = 0;
+    theNum = resultNum;
+}
+
+let clear = () => {
+    oldNum = "";
+    theNum = "";
+    viewer.textContent = "0";
+    pendNum.textContent = "";
+    equal.setAttribute('data-result', resultNum);
+};
+
+
+
+
+
+nums.forEach(num => num.addEventListener('click', setNum));
+ops.forEach(op => op.addEventListener('click', moveNum));
+equal.addEventListener('click', displayNumber);
+clearButton.addEventListener('click', clear);
 
 
 
